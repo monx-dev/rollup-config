e99ddba0ff6f1e595619c5683b0b7c4d4df4770b
Rafael Santos <rafaelnsantos1@gmail.com>
2020-11-05 20:26:43 -0300

    fix: remove peer dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
 import config from './lib/config';
 
-import { peerDependencies, main, dependencies } from './package.json';
+import { main, dependencies } from './package.json';
 
 export default config({
   dependencies,
-  peerDependencies,
   output: 'teste',
   file: 'src/index.ts',
   main,
